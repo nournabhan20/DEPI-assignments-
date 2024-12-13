725a0632be91fdc04af3488badcc076bb2efa8dd
nournabhan20 <nour.mohamed.nabhan@gmail.com>
2024-12-13 18:05:29 +0200

    Add files via upload

---
+++ SQLQuery3 nour.sql
@@ -0,0 +1,41 @@
+CREATE VIEW vw_employee_details AS
+SELECT 
+    e.fname AS first_name,
+    e.lname AS last_name,
+    d.dname AS department_name
+FROM 
+    employees e
+JOIN 
+    departments d
+ON 
+    e.department_id = d.department_id;
+	IF OBJECT_ID('vw_work_hrs', 'V') IS NOT NULL
+    DROP VIEW vw_work_hrs;
+GO
+
+CREATE VIEW vw_work_hrs AS
+SELECT 
+    e.fname AS first_name,
+    e.lname AS last_name,
+    p.pname AS project_name,
+    w.hours AS hours
+FROM 
+    employees e
+INNER JOIN 
+    work_hours w
+ON 
+    e.employee_id = w.employee_id
+INNER JOIN 
+    projects p
+ON 
+    w.project_id = p.project_id
+WHERE 
+    e.department_id = 5;
+CREATE VIEW vw_high_status_suppliers AS
+SELECT 
+    *
+FROM 
+    suppliers
+WHERE 
+    status > 15
+WITH CHECK OPTION;
